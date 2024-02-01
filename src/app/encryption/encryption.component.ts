a6e541f20794f2d19db42890b31f169623e653f6
Gokul1342441 <gokul333gn@gmail.com>
2024-02-01 17:33:35 +0530

    Ace editor

--- src/app/encryption/encryption.component.ts
+++ src/app/encryption/encryption.component.ts
@@ -1,51 +1,79 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { EncryptionService } from '../service/encryption.service';
+import * as ace from "ace-builds";
 
 @Component({
   selector: 'app-encryption',
   templateUrl: './encryption.component.html',
   styleUrl: './encryption.component.css'
 })
-export class EncryptionComponent {
+export class EncryptionComponent implements AfterViewInit {
+  @ViewChild('drawerToggle') drawerToggle!: ElementRef<HTMLInputElement>;
+  @ViewChild("editor") private editor !: ElementRef<HTMLElement>;
+
+  
   selectedMethod: string = 'AES';
   selectedOperation: string = 'encode';
-  key:string = '';
+  key: string = '';
   inputText: string = '';
   outputText: string = '';
   errorMessage: string = '';
 
   constructor(
-    private encryptservice:EncryptionService
-    ){}
+    private encryptservice: EncryptionService
+  ) { }
+
+
+  ngAfterViewInit(): void {
+    ace.config.set("fontSize", "20px");
+    ace.config.set(
+      "basePath",
+      "https://unpkg.com/ace-builds@1.4.12/src-noconflict"
+    );
+    const aceEditor = ace.edit(this.editor.nativeElement);
+    aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
+    aceEditor.setTheme("ace/theme/textmate");
+    aceEditor.session.setMode("ace/mode/yaml");
+    aceEditor.on("change", () => {
+      console.log(aceEditor.getValue());
+    });
+  }
+
+
+  closeDrawer() {
+    if (this.drawerToggle.nativeElement.checked) {
+      this.drawerToggle.nativeElement.checked = false;
+    }
+  }
 
   EncryptionAlgorithms() {
     this.errorMessage = '';
 
     try {
-      switch(this.selectedMethod) {
+      switch (this.selectedMethod) {
         case 'AES':
-          if(this.selectedOperation === 'Encryption'){
-            this.outputText = this.encryptservice.aesEncrypt(this.inputText,this.key)
-          } else if (this.selectedOperation === 'Decryption'){
-            this.outputText = this.encryptservice.aesDecrypt(this.inputText,this.key)
+          if (this.selectedOperation === 'Encryption') {
+            this.outputText = this.encryptservice.aesEncrypt(this.inputText, this.key)
+          } else if (this.selectedOperation === 'Decryption') {
+            this.outputText = this.encryptservice.aesDecrypt(this.inputText, this.key)
           }
           break;
         case 'DES':
-          if(this.selectedOperation === 'Encryption'){
-            this.outputText = this.encryptservice.desEncrypt(this.inputText,this.key)
-          } else if (this.selectedOperation === 'Decryption'){
-            this.outputText = this.encryptservice.desDecrypt(this.inputText,this.key)
+          if (this.selectedOperation === 'Encryption') {
+            this.outputText = this.encryptservice.desEncrypt(this.inputText, this.key)
+          } else if (this.selectedOperation === 'Decryption') {
+            this.outputText = this.encryptservice.desDecrypt(this.inputText, this.key)
           }
           break;
         case 'TripleDES':
-          if(this.selectedOperation === 'Encryption'){
-            this.outputText = this.encryptservice.tridesEncrypt(this.inputText,this.key)
-          } else if (this.selectedOperation === 'Decryption'){
-            this.outputText = this.encryptservice.tridesDecrypt(this.inputText,this.key)
+          if (this.selectedOperation === 'Encryption') {
+            this.outputText = this.encryptservice.tridesEncrypt(this.inputText, this.key)
+          } else if (this.selectedOperation === 'Decryption') {
+            this.outputText = this.encryptservice.tridesDecrypt(this.inputText, this.key)
           }
           break;
       }
-    } catch(error){
+    } catch (error) {
       this.errorMessage = 'Error processing input. Please check your input and try again.'
     }
   }
