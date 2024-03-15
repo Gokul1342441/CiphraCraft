4206bfccf6aa52ad1002206afe3bed2510326be9
Gokul1342441 <gokul333gn@gmail.com>
2024-03-15 12:58:44 +0530

    solve-bugs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,21 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HeaderComponent {
   @ViewChild('drawerToggle') drawerToggle!: ElementRef<HTMLInputElement>;
+  // @ViewChild('themeDropdown') themeDropdown!: ElementRef<HTMLUListElement>;
 
   closeDrawer() {
     if (this.drawerToggle.nativeElement.checked) {
       this.drawerToggle.nativeElement.checked = false;
     }
+
+ }
+
+ showModalAndCloseDrawer(modalId: string) {
+  this.closeDrawer();
+  const modal = document.getElementById(modalId) as HTMLDialogElement;
+  if (modal) {
+    modal.showModal();
   }
+}
   
 }
