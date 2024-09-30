41b3d60ed89c43b92d1ba0a6d85f19bcf146e419
Gokul1342441 <gokul333gn@gmail.com>
2024-09-30 20:34:42 +0530

    Bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { EncryptionComponent } from './encryption/encryption.component';
 import { EncoderDecoderComponent } from './encoder-decoder/encoder-decoder.component';
 const routes: Routes = [
-  {path:'encoder',component:EncoderDecoderComponent},
   { path: '', redirectTo: '/encryption', pathMatch: 'full' },
   { path: 'encryption', component: EncryptionComponent },
 ];
